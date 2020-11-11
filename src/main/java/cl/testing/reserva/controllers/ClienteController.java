862f382191facaaeb19354b6e6af745dc34b1010
Tamara <tamara.salgado1601@alumnos.ubiobio.cl>
2020-11-11 15:15:44 -0300

    base de datos y cliente

--- src/main/java/cl/testing/reserva/controllers/ClienteController.java
+++ src/main/java/cl/testing/reserva/controllers/ClienteController.java
@@ -1,4 +1,30 @@
 package cl.testing.reserva.controllers;
 
+import java.util.List;
+
+import cl.testing.reserva.model.Cliente;
+import cl.testing.reserva.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/ReservaHotelera/clientes")
 public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Cliente>> getAllClientes() {
+        List<Cliente> clientes = clienteService.getAllClientes();
+        return new ResponseEntity<List<Cliente>>(clientes,HttpStatus.OK);
+    }
+
 }
--- src/main/java/cl/testing/reserva/repository/ClienteRepository.java
+++ src/main/java/cl/testing/reserva/repository/ClienteRepository.java
@@ -1,4 +1,14 @@
 package cl.testing.reserva.repository;
 
-public interface ClienteRepository {
+import cl.testing.reserva.model.Cliente;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+@Profile("jpa")
+public interface ClienteRepository extends JpaRepository<Cliente,Integer> {
+
 }
